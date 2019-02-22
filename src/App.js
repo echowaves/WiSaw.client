966dd5bcc43cf1651d9e6dc3c2eee3c69b743b0f
dmitryame <dmitry@custommode.com>
2019-02-22 15:31:32 -0500

    initial load and redirects work now

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component, } from "react"
 
 import {
-	BrowserRouter, Route, Switch, Redirect, Link,
+	BrowserRouter, Route, Redirect,
 } from "react-router-dom"
 import PhotosComponent from './containers/PhotosComponent'
 import "./App.css"
@@ -15,43 +15,54 @@ class App extends Component {
 		}
 	}
 
+	componentDidMount() {
+		fetch('https://api.wisaw.com/photos/prev/2147483640', {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		})
+			.then(response => {
+				if (response.ok) {
+					return response.json()
+				}
+				throw new Error('Something went wrong ...')
+			})
+			.then(body => {
+				this.setState({ photoId: body.photo.id, })
+			})
+			.catch(error => alert(JSON.stringify(error)))
+	}
+
 	render() {
 		const { photoId, } = this.state
-		if (!photoId) {
-			fetch('https://api.wisaw.com/photos/prev/2147483640', {
-				method: 'GET',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-			})
-				.then(response => {
-					if (response.ok) {
-						return response.json()
-					}
-					throw new Error('Something went wrong ...')
-				})
-				.then(body => {
-					this.setState({ photoId: body.photo.id, })
-				})
-				.catch(error => alert(JSON.stringify(error)))
-
-
-			return <div>Loading</div>
-		}
-
 		return (
-			<div>
-				<BrowserRouter>
+			<BrowserRouter>
+				<div>
+					{photoId && (
+						alert(photoId)
+					)}
+					{photoId && (
+						<Redirect
+							to={{
+								pathname: `/photos/${photoId}`,
+							}}
+						/>
+					)
+					}
 					<Route path="/photos/:photoId" component={PhotosComponent} />
-				</BrowserRouter>
-				<Redirect
-					to={{
-						pathname: `/photos/${photoId}`,
-					}}
-				/>
-			</div>
+				</div>
+			</BrowserRouter>
 		)
 	}
+
+	// 	return (
+	// 		<Switch>
+	//
+	// 		<Route path="/photos/:photoId" component={PhotosComponent} />
+	// 		</Switch>
+	// 	)
+	// }
 }
 
 export default App
--- src/containers/PhotosComponent.js
+++ src/containers/PhotosComponent.js
@@ -1,24 +1,33 @@
 import React, { Component, } from "react"
 
+import {
+	BrowserRouter, Route, Redirect,
+} from "react-router-dom"
+
+
 class PhotosComponent extends Component {
 	constructor(props, context) {
 		super(props, context)
 
-		this.state = {
-			photoId: null,
-		}
+		// this.state = {
+		// 	photoId: null,
+		// }
 	}
 
 	componentDidMount() {
-		const { match, } = this.props
-		const { params, } = match
-		const { photoId, } = params
-		// alert({ photoId, })
-		this.state.photoId = photoId
+		// const { match, } = this.props
+		// const { params, } = match
+		// const { photoId, } = params
+		// // alert({ photoId, })
+		// this.state.photoId = photoId
 	}
 
 	render() {
-		const { photoId, } = this.state
+		const { match, } = this.props
+		const { params, } = match
+		const { photoId, } = params
+
+		// const { photoId, } = this.state
 		if (!photoId) {
 			return <div>...Loading...</div>
 		}
