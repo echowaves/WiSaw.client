66b77134adaef45cf4d43bc6131e0d3c5a49ea5e
dmitryame <dmitry@custommode.com>
2019-02-23 10:44:41 -0500

    quick and durty first version is readdy

--- src/containers/PhotosComponent.js
+++ src/containers/PhotosComponent.js
@@ -77,44 +77,97 @@ class PhotosComponent extends Component {
 				this.setState({ photo: body.photo, })
 			})
 			.catch(error => this.setState({ photo: null, }))
+
+
+		fetch(`https://api.wisaw.com/photos/${photoId}/comments`, {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		})
+			.then(response => {
+				if (response.ok) {
+					return response.json()
+				}
+				throw new Error('Something went wrong ...')
+			})
+			.then(body => {
+				this.setState({ comments: body.comments, })
+			})
+			.catch(error => this.setState({ photo: null, }))
 	}
 
 	render() {
-		const { photo, prevPhoto, nextPhoto, } = this.state
+		const {
+			photo, prevPhoto, nextPhoto, comments,
+		} = this.state
 
 		return (
 			<div>
-				<div>
+				<div style={{
+					display: 'flex',
+					justifyContent: 'center',
+				}}>
+					{prevPhoto
+					&& (
+						<div style={{ margin: '10px', }}>
+							<Link to={`/photos/${prevPhoto.id}`} onClick={<Redirect from="/" to={`/photos/${prevPhoto.id}`} />}>prev</Link>
+						</div>
+					)
+					}
+					{nextPhoto
+						&& (
+							<div style={{ margin: '10px', }}>
+								<Link to={`/photos/${nextPhoto.id}`} onClick={<Redirect from="/" to={`/photos/${nextPhoto.id}`} />}>next</Link>
+							</div>
+						)
+					}
+				</div>
+				<div align="center">
 					{photo && (
 						<img
 							src={photo.getImgUrl}
 							alt=""
 							style={{
-								width: 'auto',
-								height: '70%',
-								position: 'absolute',
-								top: 50,
-								left: 50,
+								height: '600px',
+								justifyContent: 'center',
+								alignItems: 'center',
 							}}
 
 						/>
 					)}
 				</div>
-				<div>
-					{nextPhoto
-						&& (
-							<div>
-								<Link to={`/photos/${nextPhoto.id}`} onClick={<Redirect from="/" to={`/photos/${nextPhoto.id}`} />}>next</Link>
-							</div>
-						)
-					}
-					{prevPhoto
-						&& (
-							<div>
-								<Link to={`/photos/${prevPhoto.id}`} onClick={<Redirect from="/" to={`/photos/${prevPhoto.id}`} />}>prev</Link>
-							</div>
-						)
-					}
+				<div style={{
+					display: 'flex',
+					justifyContent: 'center',
+				}}>
+					<div align="center" style={{ margin: '10px', }}>
+						{comments && comments.length > 0 && (
+							<div>Comments:{comments.length}</div>
+						)}
+					</div>
+					<div align="center" style={{ margin: '10px', }}>
+						{photo && photo.likes > 0 && (
+							<div>Likes:{photo.likes}</div>
+						)}
+					</div>
+				</div>
+				<div style={{
+					display: 'flex',
+					flexDirection: 'column',
+					justifyContent: 'center',
+				}}>
+					{comments && (
+						<div align="center" style={{ margin: '10px', }}>
+
+							{comments.map((comment, i) => (
+								<div
+									key={comment.id}
+									style={{ margin: '10', }}>{comment.comment}
+								</div>
+							))}
+						</div>
+					)}
 				</div>
 			</div>
 		)
