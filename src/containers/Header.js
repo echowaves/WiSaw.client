ecdf9110717797cb4d28b4c2ae17257b92bdd574
dmitryame <dmitry@custommode.com>
2021-04-12 16:48:47 -0400

    slightly better alignment of the logo

--- src/containers/Header.js
+++ src/containers/Header.js
@@ -12,14 +12,21 @@ const Header = () => {
     <header
       id="header"
       style={{
-        width: '100%',
-        textAlign: 'center',
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
       }}>
       <h1
+        className="align-baseline"
         style={{
           fontFamily: 'verdana',
           fontSize: '3vmax',
-        }}><a href="https://www.wisaw.com"><img width="50" height="50" src="/android-chrome-192x192.png" alt="wisaw logo" /></a> What I Saw <a href="https://www.wisaw.com">#wisaw</a>
+        }}>
+        <a href="https://www.wisaw.com">
+          <img
+            width="55" height="55" src="/android-chrome-192x192.png" alt="wisaw logo"
+          />
+        </a> What I Saw <a href="https://www.wisaw.com">#wisaw</a>
       </h1>
     </header>
   )
