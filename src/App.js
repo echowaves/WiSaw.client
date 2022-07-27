e2e8e0e3110162883ab866713b61f9f19a9da927
dmitryame <dmitry@custommode.com>
2022-07-27 12:27:19 -0400

    lazyWithRetry

--- src/App.js
+++ src/App.js
@@ -7,12 +7,46 @@ import {
 
 import "./App.css"
 
-const PhotosComponent = lazy(() => import('./containers/PhotosComponent'))
-const SearchComponent = lazy(() => import('./containers/SearchComponent'))
+const lazyWithRetry = (componentImport) => lazy(async () => {
+  const pageHasAlreadyBeenForceRefreshed = JSON.parse(
+    window.localStorage.getItem(
+      'page-has-been-force-refreshed'
+    ) || 'false'
+  )
 
-const NoMatch = lazy(() => import('./containers/NoMatch'))
-const Footer = lazy(() => import('./containers/Footer'))
-const Header = lazy(() => import('./containers/Header'))
+  try {
+    const component = await componentImport()
+
+    window.localStorage.setItem(
+      'page-has-been-force-refreshed',
+      'false'
+    )
+
+    return component;
+  } catch (error) {
+    if (!pageHasAlreadyBeenForceRefreshed) {
+      // Assuming that the user is not on the latest version of the application.
+      // Let's refresh the page immediately.
+      window.localStorage.setItem(
+        'page-has-been-force-refreshed',
+        'true'
+      )
+      return window.location.reload()
+    }
+
+    // The page has already been reloaded
+    // Assuming that user is already using the latest version of the application.
+    // Let's let the application crash and raise the error.
+    throw error
+  }
+})
+
+const PhotosComponent = lazyWithRetry(() => import('./containers/PhotosComponent'))
+const SearchComponent = lazyWithRetry(() => import('./containers/SearchComponent'))
+
+const NoMatch = lazyWithRetry(() => import('./containers/NoMatch'))
+const Footer = lazyWithRetry(() => import('./containers/Footer'))
+const Header = lazyWithRetry(() => import('./containers/Header'))
 
 const App = () => (
   <HelmetProvider>
