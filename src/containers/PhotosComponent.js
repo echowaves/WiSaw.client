167f502ddb44cd3d154429fefffe8deed2804a9d
dmitryame <dmitry@custommode.com>
2024-08-02 13:22:42 -0400

    reevrting

--- src/containers/PhotosComponent.js
+++ src/containers/PhotosComponent.js
@@ -489,7 +489,6 @@ this methid will fetch image into cache -- will work super fast on next call to 
 
           {currPhoto?.photo?.video !== true && (
             <img
-              fetchPriority="high"
               width={`${dimensions.width}`}
               height={`${dimensions.height}`}
               className='mainImage'
